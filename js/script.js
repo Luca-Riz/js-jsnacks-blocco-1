5275d2d17604948a96bd0d546e6c1f64f617ff26
Luca-Riz <luca983@gmail.com>
2021-06-01 16:01:50 +0200

    ad snack 2 soluzione con for

--- js/script.js
+++ js/script.js
@@ -3,16 +3,16 @@
 // L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
 // Se uguale lunghezza dire sono uguali e stamparle entrambe.
 
-var parola1 = prompt('inserisci parola 1')
-var parola2 = prompt('inserisci parola 2')
+// var parola1 = prompt('inserisci parola 1')
+// var parola2 = prompt('inserisci parola 2')
 
-if (parola1.length < parola2.length) {
-    document.getElementById('parole').innerHTML = parola1 + ', ' + parola2;
-}   else if (parola1.length > parola2.length) {
-    document.getElementById('parole').innerHTML = parola2 + ', ' + parola1;
-}   else {
-    document.getElementById('parole').innerHTML = 'Le parole sono lunghe uguali e sono: '+ parola1 + ', ' + parola2;
-}
+// if (parola1.length < parola2.length) {
+//     document.getElementById('parole').innerHTML = parola1 + ', ' + parola2;
+// }   else if (parola1.length > parola2.length) {
+//     document.getElementById('parole').innerHTML = parola2 + ', ' + parola1;
+// }   else {
+//     document.getElementById('parole').innerHTML = 'Le parole sono lunghe uguali e sono: '+ parola1 + ', ' + parola2;
+// }
 
 // ----- fine JSnack1 -----
 
@@ -22,7 +22,16 @@ if (parola1.length < parola2.length) {
 // Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti. Esegui questo programma in due versioni, con il for e con il while.
 // Variante: stampare solo i numeri pari (modificato) 
 
+//soluzione con 'for'
+var somma = 0;
 
+for (var i=0; i<5; i++) {
+    var numero = parseInt(prompt('inserisci un numero'))
+    somma += numero;
+    console.log(somma);
+};
+
+document.getElementById('somma').innerHTML = somma
 
 // ----- fine JSnack2 -----
 
