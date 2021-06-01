90c2af0cde1b989aa6a70f31aa67427d0c0db0d9
Luca-Riz <luca983@gmail.com>
2021-06-01 15:43:51 +0200

    add snack 1

--- js/script.js
+++ js/script.js
@@ -3,7 +3,16 @@
 // L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
 // Se uguale lunghezza dire sono uguali e stamparle entrambe.
 
+var parola1 = prompt('inserisci parola 1')
+var parola2 = prompt('inserisci parola 2')
 
+if (parola1.length < parola2.length) {
+    document.getElementById('parole').innerHTML = parola1 + ', ' + parola2;
+}   else if (parola1.length > parola2.length) {
+    document.getElementById('parole').innerHTML = parola2 + ', ' + parola1;
+}   else {
+    document.getElementById('parole').innerHTML = 'Le parole sono lunghe uguali e sono: '+ parola1 + ', ' + parola2;
+}
 
 // ----- fine JSnack1 -----
 
