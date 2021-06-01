ba810c5f51b99177d2054d624a341355c9b6fc03
Luca-Riz <luca983@gmail.com>
2021-06-01 16:35:58 +0200

    add snack 3

--- js/sn3.js
+++ js/sn3.js
@@ -1,2 +1,13 @@
 // 20 minuti
 // Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
+
+var numArray = [];
+
+for (var i=0; i<6; i++){
+    var num = parseInt(prompt('inserisci un numero'));
+    if (num % 2 != 0){
+        numArray.push(num);
+        // console.log(numArray);
+    }
+}
+document.getElementById('numArray').innerHTML = numArray;
