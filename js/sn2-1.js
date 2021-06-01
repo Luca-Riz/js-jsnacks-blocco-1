71db953f76a6411e23e2e8d383df9d9baf3789d7
Luca-Riz <luca983@gmail.com>
2021-06-01 16:45:46 +0200

    add end of project

--- js/sn2-1.js
+++ js/sn2-1.js
@@ -1,10 +1,9 @@
 // ----- JSnack2 -----
 
-// 25 minuti
 // Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti. Esegui questo programma in due versioni, con il for e con il while.
-// Variante: stampare solo i numeri pari 
 
 //soluzione con 'for'
+
 var somma = 0;
 
 for (var i=0; i<5; i++) {
--- js/sn2-3.js
+++ js/sn2-3.js
@@ -1,4 +1,9 @@
+// ----- JSnack2 -----
+
+// Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri inseriti. Esegui questo programma in due versioni, con il for e con il while.
+
 //Variante: sommare solo i numeri pari
+
 var somma = 0;
 
 for (var i=0; i<5; i++) {
