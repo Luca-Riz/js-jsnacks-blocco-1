3538d82f1c6b7eed6664b5625e1c93add93cc75b
Luca-Riz <luca983@gmail.com>
2021-06-01 16:43:04 +0200

    add snack 4

--- js/sn4.js
+++ js/sn4.js
@@ -1,3 +1,18 @@
 // ----- JSnack4 -----
 
 // In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+var lista = ['Jay Gatsby', 'Nick Carraway', 'Daisy', 'Tom', 'Myrtle', 'Il meccanico'];
+
+var nomeUtente = prompt('Inserisci il tuo nome');
+var ok = false;
+
+for(i = 0; i < lista.length; i++){
+    if (lista[i] == nomeUtente){
+        document.getElementById('gatsby').innerHTML = 'Il tuo nome è nella lista, puoi partecipare.';
+        ok = true;
+    }
+}
+if (ok == false){
+    document.getElementById('gatsby').innerHTML = 'Il tuo nome non è nella lista, non puoi partecipare.';
+}
